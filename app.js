1fedc18ad6b0eb1641d4a3f1604b1cc378af29a0
Kevin Potts <fancydude@Kevins-MacBook-Pro-2.local>
2018-09-04 13:05:51 -0600

    cleaned up code

--- app.js
+++ app.js
@@ -7,36 +7,36 @@ let arr = []
 
 document.addEventListener("DOMContentLoaded", function (event) {
   fetch('https://dino-1-server.herokuapp.com/')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(result) {
-    for (let i = 0; i < result.length; i++) {
-            let listElement = document.createElement('li')
-            let title = result[i]['title']
-            let pay = result[i]['pay']
-            let description = result[i]['description']
-            let interested = result[i]['interested'].length
-            console.log(title, pay, description)
-            listElement.innerHTML = `<h4>${title}</h4>
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      for (let i = 0; i < result.length; i++) {
+        let listElement = document.createElement('li')
+        let title = result[i]['title']
+        let pay = result[i]['pay']
+        let description = result[i]['description']
+        let interested = result[i]['interested'].length
+        console.log(title, pay, description)
+        listElement.innerHTML = `<h4>${title}</h4>
             <small>${pay}</small>
             <p>${description}</p>
             <small> ${interested} Dinos are interested in this position</small>`
-            listings.appendChild(listElement)
-    }
-  })
+        listings.appendChild(listElement)
+      }
+    })
 
-  })
-  submit.addEventListener('click', function (element) {
-    element.preventDefault()
-    let newElement = document.createElement('li')
+})
+submit.addEventListener('click', function (element) {
+  element.preventDefault()
+  let newElement = document.createElement('li')
 
-    newElement.innerHTML = `<h4>${title.value}</h4>
+  newElement.innerHTML = `<h4>${title.value}</h4>
     <small>${pay.value}</small>
     <p>${description.value}</p>`
-    listings.appendChild(newElement)
+  listings.appendChild(newElement)
 
-  })
-  
+})
+
 
 
